8cbcecbd39b82e2d57f5b2fa67bda45b37373a39
AnupParajuli09 <anup.parajuli@wsu.edu>
2025-02-22 16:05:07 -0800

    Added Comments to store variables

--- Build_Model/store.py
+++ Build_Model/store.py
@@ -1,4 +1,4 @@
-
+## This Script is used to store the optimization variables in a dictionary after solving
 from pyomo.environ import *
 import numpy as np
 
@@ -6,13 +6,13 @@ def store_results(model):
     modelVals = {}
 
     # Initialize containers for each variable
-    modelVals['P_subs'] = {}
-    modelVals['P'] = {}
-    modelVals['Pe_c'] = {}
-    modelVals['Pe_d'] = {}
-    modelVals['P_c'] = {}
-    modelVals['P_d'] = {}
-    modelVals['B'] = {}
+    modelVals['P_subs'] = {} ## Initializing dictionary placeholder to store substation power flow for different time periods
+    modelVals['P'] = {} ## Initializing dictionary placeholder to stores active power flows for different time periods and different lines
+    modelVals['Pe_c'] = {} ## Initializing dictionary placeholder to store edo charging power for different time periods and edo nodes
+    modelVals['Pe_d'] = {} ## Initializing dictionary placeholder to store edo discharging power for different time periods and edo nodes
+    modelVals['P_c'] = {} ## Initializing dictionary placeholder to store battery charging power for different time periods and battery nodes
+    modelVals['P_d'] = {} ## Initializing dictionary placeholder to store battery discharging power for different time periods and battery nodes
+    modelVals['B'] = {} ## Initializing dictionary placeholder to store battery state of charge for different time periods and battery nodes
 
     # Store the optimization results for each variable
     for t in model.Tset:
