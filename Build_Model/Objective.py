06975b5fca6c4f56b89ae4e1c260116ba3b4cd88
AnupParajuli09 <anup.parajuli@wsu.edu>
2025-02-22 17:08:37 -0800

    Added Comments to objective functions

--- Build_Model/Objective.py
+++ Build_Model/Objective.py
@@ -11,8 +11,8 @@ def substation_power_minimize(model):
 def substation_power_minimize_with_discharge_cost(model):
     cost = model.cost
     sub_power = sum(model.P_subs[t] for t in model.Tset) ## Total substation power for all time periods
-    bat_dis_cost = sum(model.P_d[t,j] * cost[t] for t in model.Tset for j in model.Bset) ## associated discharging costs of battery nodes
-    edo_dis_cost = sum(model.Pe_d[t, j] * cost[t] for t in model.Tset for j in model.Eset) ## associated discharging costs of edo nodes
+    bat_dis_cost = sum(model.P_d[t,j] * cost[t] /2 for t in model.Tset for j in model.Bset) ## associated discharging costs of battery nodes
+    edo_dis_cost = sum(model.Pe_d[t, j] * cost[t] /3 for t in model.Tset for j in model.Eset) ## associated discharging costs of edo nodes
     scd_term = sum((1 - model.n_c) * model.P_c[t, j] + ((1 / model.n_d) - 1) * model.P_d[t, j] for t in model.Tset for j in model.Bset) ## terms to get rid of simultaneous charging and discharging of battery
     alpha = 1e-3
 
